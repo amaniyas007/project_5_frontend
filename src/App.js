adb8eb9647c495a7a0ec38bc39c9a233245784c7
amanniyas007 <amanniyas007gmail.com>
2023-06-19 11:29:12 +0530

    Login,SignUp,MainPage,Header, Category components added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import './App.css';
-import React from 'react'
+import "./App.css";
+// import { createContext, useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainPage from "./components/screens/MainPage"
+import NoMatch from "./components/screens/NoMatch";
+import LogIn from "./components/screens/LogIn";
+import SignUp from "./components/screens/SignUp";
+import Category from "./components/screens/Category";
+import Store from "./components/context/store";
+
+
 
 function App() {
-  return (
-    <div>App</div>
-  )
+
+    return (
+        <>
+            <Store>
+                <Router>
+                    <Routes>
+                        <Route path="/home" element={<MainPage />} />
+                        <Route path="/category" element={<Category />} />
+                        <Route path="/login" element={<LogIn />} />
+                        <Route path="/signup/" element={<SignUp />} />
+                        <Route path="*" element={<NoMatch />} />
+                    </Routes>
+                </Router>
+            </Store>
+        </>
+    );
 }
 
-export default App
+export default App;
